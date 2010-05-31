98c20cd135430bbd6428fc24c2340ac10ed2f655
John L Mitchell <johnlmitchell@gmail.com>
2010-05-31 13:37:41 -0700

    unicode works

--- bin/dzen.py
+++ bin/dzen.py
@@ -4,16 +4,18 @@
 jmrip -- rip DVD, encode into single ISO
 '''
 
-import codecs, os, re, sys, time
+import codecs, sys, time
 from subprocess import PIPE, Popen
-from itertools import ifilter, imap
 from nose.tools import eq_ as eq
 
 # http://www.fileformat.info/info/unicode/block/geometric_shapes/list.htm
 
 class DzenStatus(dict):
     CONFIG = dict(
-        DZEN      = 'dzen2 -ta 1 -tw 190 -fg %s -bg %s -p',
+        DZEN      = ("dzen2 -ta 1 -tw 190 -fg %(WORK_FG)s -bg %(WORK_BG)s"
+                     " -p -fn '%(FONT)s'"
+                     ),
+        FONT	 = '-*-DejaVu Sans Mono-medium-r-normal-*',
         PAUSE_FG  = "black",
         PAUSE_BG  = "green",
         PROGRESS  = "^fg(%s)^bg(%s)Cycle %d/%d: ",
@@ -22,20 +24,21 @@ class DzenStatus(dict):
         SET_FG    = "^fg(%s)",
         WORK_BG   = "black",
         WORK_FG   = "yellow",
-        DOTTED_CIRCLE = u'\x25db',
-        DONE1 = u'\x25d4',
+        # DOTTED_CIRCLE = u'\u25db', # X: envelope!
+        DONE0 = '-',
+        DONE25 = u'\u25d4',
         )
 
     def __init__(self):
         super(DzenStatus,self).__init__(self.CONFIG)
         self.pipe = Popen(
-            self.DZEN % (self.WORK_FG, self.WORK_BG),
+            self.DZEN % self,
             shell=True, stdin=PIPE,
             )
         self.dzen = codecs.getwriter('utf-8')(self.pipe.stdin)
 
     def sendline(self, line):
-        
+        print u'dzen: %s' % line
         self.dzen.write(line+'\n')
         self.dzen.flush()
 
@@ -44,5 +47,9 @@ class DzenStatus(dict):
 
 def test_status():
     st = DzenStatus()
-    st.sendline(u'beer\u25d4')
-    time.sleep(5)
+    st.sendline(u'starting: %s' % st.DONE0)
+    time.sleep(2)
+    st.sendline(u'25%% %s' % st.DONE25)
+    time.sleep(2)
+    # st.sendline('50% %s' % st.DOTTED_CIRCLE)
+    # time.sleep(2)

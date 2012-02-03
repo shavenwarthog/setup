a020c25b17aaf86c33cf97f6eb5487972ffc2c6b
John Mitchell <johnlmitchell@gmail.com>
2012-02-03 10:49:10 -0800

    cleanup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,84 +14,41 @@
 
 ;; :::::::::::::::::::::::::::::::::::::::::::::::::: PACKAGING
 
-;; (when nil
-;;   (require 'package)
+(setq package-archives 
+      '(("ELPA" . "http://tromey.com/elpa/") 
+	("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")))
 
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
-                          ("gnu" . "http://elpa.gnu.org/packages/")
-                          ("marmalade" . "http://marmalade-repo.org/packages/")))
-  
-;; )
-;; (package-install 'bookmark+)
-(when nil
-  (add-to-list 'load-path "~/.emacs.d/elpa/bookmark+-20111214")
-  (require 'bookmark+))
-
-;; http://avdi.org/devblog/2011/10/06/required-packages-emacs-reboot-12/
-(when nil
-  (setq package-archives
-	'(("gnu" . "http://elpa.gnu.org/packages/")
-	  ("marmalade" . "http://marmalade-repo.org/packages/")
-	  ("Tromey" . "http://tromey.com/elpa/")))
-  (package-initialize)
-  (setq abg-required-packages
-	(list 'xml-rpc 'magit 'gh))
-  (dolist (package abg-required-packages)
-    (when (not (package-installed-p package))
-      (package-refresh-contents)
-      (package-install package))))
-
-;; :::::::::::::::::::::::::::::::::::::::::::: KEYS
-
-;; C-x r m		bookmark-set
-;; M-s h r		highlight-regex
 
 ;; :::::::::::::::::::::::::::::::::::::::::::::::::: PERL
 ;
-;; (defun jm-test-function ()
-;;   (interactive)
-
 ;; https://github.com/bricoleurs/bricolage/wiki/coding-standards ?
 
-(require 'cperl-mode)
-(fset 'perl-mode 'cperl-mode)
+(when t
+  (require 'cperl-mode)
+  (fset 'perl-mode 'cperl-mode)
 
-(require 'perldoc)
+  (require 'perldoc)
 
-(add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
+  (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
 
-(defun jmc-cperl-mode-hook ()
-  ;; SendGrid ~ GNU style with braces aligned with control words
-  (cperl-set-style "GNU")
-  (setq cperl-continued-brace-offset -2
-	indent-tabs-mode nil
-        c-basic-offset 2
-        cperl-indent-level 2)
-  (show-paren-mode 1)
-  (local-set-key (kbd "C-h f") 'perldoc-at-point))
+  (defun jmc-cperl-mode-hook ()
+    ;; SendGrid ~ GNU style with braces aligned with control words
+    (cperl-set-style "GNU")
+    (setq cperl-continued-brace-offset -2
+	  indent-tabs-mode nil
+	  c-basic-offset 2
+	  cperl-indent-level 2)
+    (show-paren-mode 1)
+    (local-set-key (kbd "C-h f") 'perldoc-at-point))
+  
+  (setq cperl-mode-hook 'jmc-cperl-mode-hook))
 
-(setq cperl-mode-hook 'jmc-cperl-mode-hook)
 
+;; :::::::::::::::::::::::::::::::::::::::::::::::::: TWEAKS
 
-  ;; (defvaralias 'c-basic-offset 'tab-width)
-  ;; (defvaralias 'cperl-indent-level 'tab-width)
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 
-;; (make-variable-buffer-local 'tab-width)
-
-;; sudo aptitude install emacs-goodies-el
-;; (require 'info-look)
-;; (require 'bookmark+)
-
-  ;; (require 'smart-tabs)
-  ;; (smart-tabs-advice cperl-indent-line cperl-indent-level)
-;; (eval-after-load "compile" '(require 'compilation-perl))
-
-;
-;
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: TWEAKS
-;; ;
-
-;; (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
 
@@ -100,19 +57,8 @@
 (setq use-file-dialog nil)
 (tool-bar-mode -1)
 
-;; (server-start t)
-;; ;; (desktop-save-mode 1)
-;; (setq redisplay-dont-pause t)
-
-
-;; ;; find-tag is case sensitive for Python
-;; ;; (add-hook 'python-mode-hook (lambda () (setq case-fold-search nil)))
-;; (setq tags-case-fold-search nil)
-;; ;
-
 
 ;; :::::::::::::::::::::::::::::::::::::::::::::::::: MODULES
-
 
 (when (require 'ido)
   (ido-mode t)
@@ -124,30 +70,24 @@
 
 
 (require 'python)
-(when nil
+(when t
   (load "~/src/flynote/flynote" t))
 ;;   (message "yay"))
 
 
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: GNUPLOT
-;; ;
-;; (when (load "gnuplot" t)
-;;   (defun jmc-call-gnuplot-on-buffer ()
-;;     (interactive)
-;;     (save-some-buffers t)
-;;     (call-gnuplot-on-buffer))
-;;   (add-to-list 'auto-mode-alist '("\\.plt$" . gnuplot-mode))
-;;   ;; (define-key gnuplot-mode-map (kbd "C-c C-c") 'jmc-call-gnuplot-on-buffer)
-;;   (define-key gnuplot-mode-map (kbd "C-S-<return>") 'jmc-call-gnuplot-on-buffer))
-;; ;
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: EXTERNAL
-;; ;
-;; ;; (load "~/src/sunlight/jmcompile")
-;; ; ;; (load "~/src/rudel/rudel-loaddefs.el" t)
+; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: GNUPLOT
+;
+(when (load "gnuplot" t)
+  (defun jmc-call-gnuplot-on-buffer ()
+    (interactive)
+    (save-some-buffers t)
+    (call-gnuplot-on-buffer))
+  (add-to-list 'auto-mode-alist '("\\.plt$" . gnuplot-mode))
+  ;; (define-key gnuplot-mode-map (kbd "C-c C-c") 'jmc-call-gnuplot-on-buffer)
+  (define-key gnuplot-mode-map (kbd "C-S-<return>") 'jmc-call-gnuplot-on-buffer))
 
-;; ;
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: KEYS
-;; ;
+
+;; :::::::::::::::::::::::::::::::::::::::::::::::::: KEYS
 
 (global-set-key (kbd "C-<return>") 'recompile)
 (global-set-key (kbd "C-S-<return>") 'recompile)
@@ -169,16 +109,9 @@
 
 (setq compilation-ask-about-save nil)
 
-
-;; ;; (require 'ipython)
-;; ;; (define-key python-mode-map (kbd "C-S-<return>") 'jmc-test-something)
-;; ;
-;; ;
-;; ;
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: CUSTOM
+;; :::::::::::::::::::::::::::::::::::::::::::::::::: CUSTOM
 
 ; ;; :height 120 "this is an integer in units of 1/10 point"
-; ;; :width normal
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -187,25 +120,7 @@
  '(default ((t (:background "black" :foreground "white" :height 110)))))
 
 
-;; ; ;; :::::::::::::::::::::::::::::::::::::::::::::::::: TRAMP
-;; ;
-(setq tramp-mode nil)
-;; ; (setq tramp-mode t
-;; ;       tramp-verbose 4)
-;; ; ;; (find-file "/dev6-md2.sendgrid.net:work/kamta/Makefile")
-;; ;
-;; (custom-set-variables
-;;   ;; custom-set-variables was added by Custom.
-;;   ;; If you edit it by hand, you could mess it up, so be careful.
-;;   ;; Your init file should contain only one such instance.
-;;   ;; If there is more than one, they won't work right.
-;;  '(safe-local-variable-values (quote ((test-case-name . twisted\.mail\.test\.test_smtp) (test-case-name . twisted\.test\.test_process) (test-case-name . twisted\.test\.test_internet\,twisted\.internet\.test\.test_posixbase)))))
 
-;; (server-start)
-
-
-;; (require 'compile)
-;; (setq  compilation-search-path '("." "bin"))
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -213,3 +128,59 @@
   ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "~/.emacs.bmk")
  '(inhibit-startup-screen t))
+
+
+;; :::::::::::::::::::::::::::::::::::::::::::::::::: HISTORICAL
+
+
+;; )
+;; (package-install 'bookmark+)
+(when nil
+  (add-to-list 'load-path "~/.emacs.d/elpa/bookmark+-20111214")
+  (require 'bookmark+))
+
+;; http://avdi.org/devblog/2011/10/06/required-packages-emacs-reboot-12/
+(when nil
+  (setq package-archives
+	'(("gnu" . "http://elpa.gnu.org/packages/")
+	  ("marmalade" . "http://marmalade-repo.org/packages/")
+	  ("Tromey" . "http://tromey.com/elpa/")))
+  (package-initialize)
+  (setq abg-required-packages
+	(list 'xml-rpc 'magit 'gh))
+  (dolist (package abg-required-packages)
+    (when (not (package-installed-p package))
+      (package-refresh-contents)
+      (package-install package))))
+
+
+
+
+  ;; (defvaralias 'c-basic-offset 'tab-width)
+  ;; (defvaralias 'cperl-indent-level 'tab-width)
+
+;; (make-variable-buffer-local 'tab-width)
+
+;; sudo aptitude install emacs-goodies-el
+;; (require 'info-look)
+;; (require 'bookmark+)
+
+  ;; (require 'smart-tabs)
+  ;; (smart-tabs-advice cperl-indent-line cperl-indent-level)
+;; (eval-after-load "compile" '(require 'compilation-perl))
+
+;; (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; (server-start t)
+;; ;; (desktop-save-mode 1)
+;; (setq redisplay-dont-pause t)
+
+
+;; ;; find-tag is case sensitive for Python
+;; ;; (add-hook 'python-mode-hook (lambda () (setq case-fold-search nil)))
+;; (setq tags-case-fold-search nil)
+;; ;
+
+
+;; (require 'compile)
+;; (setq  compilation-search-path '("." "bin"))
